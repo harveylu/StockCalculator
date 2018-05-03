7b375a54a204d709aa0acc5b85803cdb70f7104a
harveylu <32376019+harveylu@users.noreply.github.com>
2018-05-03 15:52:20 -0700

    Add files via upload
    Upload an on-going project to Github

---
+++ boards/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def result(request):
+    return render(request, 'result.html')
