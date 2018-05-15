2999e114627f185273d137163b9850f0ea429963
harvey <minglei.lu@sjsu.edu>
2018-05-15 16:53:19 -0700

    Django1.11.13

--- myproject/settings.py
+++ myproject/settings.py
@@ -54,6 +54,9 @@ ROOT_URLCONF = 'myproject.urls'
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
